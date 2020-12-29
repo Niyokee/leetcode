22cdd9ec9fd1098307a57dc0f7bdfe4f756b725e
noaki-k <knk43nok@gmail.com>
2020-12-29 17:55:30 +0900

    mod: 268. Missing Number

--- 268. Missing Number.go
+++ 268. Missing Number.go
@@ -17,3 +17,22 @@ func missingNumber(nums []int) int {
 	}
 	return 0
 }
+
+/*
+上の回答の２つのif文がforの中に入っている必要はないので外に出した。
+*/
+func missingNumber1(nums []int) int {
+	sort.Ints(nums)
+	// ensure that 0 is the first index
+	if nums[0] != 0 {
+		return 0
+	} else if nums[len(nums)-1] != len(nums) {
+		return len(nums)
+	}
+	for i, n := range nums {
+		if n+1 != nums[i+1] {
+			return n + 1
+		}
+	}
+	return 0
+}
